113320ec6980e6a4bf86a074823f93bc73cfc6de
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-10-11 14:29:23 -0300

    Update questao04L6.c

--- questao04L6.c
+++ questao04L6.c
@@ -1,3 +1,7 @@
+//4. Faca um programa q pergunte ao usuário o numero de alunos a ser lido. O tamanho dos vetores será o numero informado pelo usuário.
+//Armazene num vetor as notas G1 destes alunos; num outro vetor, armazene as notas G2 destes alunos. Ambas notas, G1 e G2, são informadas
+//pelo usuário. Calcule a media aritmética destes alunos e armazene num terceiro vetor. Ao final, mostre as 3 notas dos alunos.
+
 #include <stdio.h>
 
 int main() {
